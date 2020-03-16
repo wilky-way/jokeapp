d89aefefc2ce2b3fde8112d03af8305f263d26f2
wilky-way <wilkyway901@gmail.com>
2020-03-16 09:51:20 -0500

    Create Spring MVC Controller

---
+++ src/main/java/com/wilky/joke/jokeapp/controllers/JokeController.java
@@ -0,0 +1,24 @@
+package com.wilky.joke.jokeapp.controllers;
+
+import com.wilky.joke.jokeapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+    private JokeService jokeService;
+
+    @Autowired //optional
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke()); //joke attribute named joke
+
+        return "chucknorris";//return viewname
+    }
+}
